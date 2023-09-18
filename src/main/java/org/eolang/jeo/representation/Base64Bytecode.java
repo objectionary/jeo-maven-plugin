002ae0b5713b83beec45d279ee9d763aad663ce4
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-09-18 15:04:11 +0300

    feat(#55): remove the puzzle for #55 issue

--- src/main/java/org/eolang/jeo/representation/Base64Bytecode.java
+++ src/main/java/org/eolang/jeo/representation/Base64Bytecode.java
@@ -31,10 +31,6 @@ import java.util.Base64;
  * Converts bytecode to Base64 string.
  *
  * @since 0.1.0
- * @todo #37:90min Add unit test for Base64Bytecode class.
- *  The test should check all the methods of the {@link Base64Bytecode} class.
- *  Don't forget to test corner cases.
- *  When the test is ready, remove this puzzle.
  */
 class Base64Bytecode {
 
