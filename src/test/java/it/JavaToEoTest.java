63bf3e6b61e7a8b5befb8028da1864dc3967f411
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-09-20 12:29:21 +0300

    feat(#81): Parse eo from resources

---
+++ src/test/java/it/JavaToEoTest.java
@@ -0,0 +1,25 @@
+package it;
+
+import org.cactoos.io.OutputTo;
+import org.cactoos.io.ResourceOf;
+import org.cactoos.text.TextOf;
+import org.eolang.parser.Syntax;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JavaToEoTest {
+
+    @Test
+    void compilesJavaAndTranspilesBytecodeToEo() throws Exception {
+        final ResourceOf resourceOf = new ResourceOf(
+            "transpilation/eo/ch_4_types_values_variables/sec_4_2_primitive_types_and_values/sec_4_2_1_integral_types_and_values/MethodByte.eo"
+        );
+        System.out.println(new TextOf(resourceOf).asString());
+        new Syntax(
+            "scenario",
+            resourceOf,
+            new OutputTo(System.out)
+        ).parse();
+        Assertions.assertTrue(true);
+    }
+}
