7f167623d6a4b9a8d9593622510cbd3c6daab4c6
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-09-26 11:56:28 +0300

    feat(#96): add package-info.java for asm tests

--- src/test/java/org/eolang/jeo/representation/asm/package-info.java
+++ src/test/java/org/eolang/jeo/representation/asm/package-info.java
@@ -21,38 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package org.eolang.jeo.representation.asm;
-
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Test;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.Opcodes;
-
 /**
- * Test case for {@link ClassName}.
- * @since 0.1.0
+ * This package contains tests for classes in {@link org.eolang.jeo.representation.asm} package.
  */
-class ClassNameTest {
-
-    @Test
-    void retrievesClassName() {
-        final ClassName name = new ClassName();
-        final ClassWriter writer = new ClassWriter(0);
-        writer.visit(
-            Opcodes.ASM9,
-            Opcodes.ACC_PUBLIC,
-            "representation/asm/ClassNameTest",
-            null,
-            "java/lang/Object",
-            null
-        );
-        new ClassReader(writer.toByteArray()).accept(name, 0);
-        MatcherAssert.assertThat(
-            "Can't retrieve class name, or it's incorrect",
-            name.asString(),
-            Matchers.equalTo("representation.asm.ClassNameTest")
-        );
-    }
-}
+package org.eolang.jeo.representation.asm;
