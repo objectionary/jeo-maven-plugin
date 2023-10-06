903645bdf09a81e61d558be7ff35dc41c892110b
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-10-06 15:55:20 +0300

    feat(#139): Add DirectivesTuple class

---
+++ src/main/java/org/eolang/jeo/representation/asm/DirectivesTuple.java
@@ -0,0 +1,73 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2023 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.eolang.jeo.representation.asm;
+
+import java.util.Iterator;
+import org.xembly.Directive;
+import org.xembly.Directives;
+
+/**
+ * Tuple representation as Xembly directives.
+ *
+ * @since 0.1.0
+ */
+public class DirectivesTuple implements Iterable<Directive> {
+
+    /**
+     * Tuple name.
+     */
+    private final String name;
+
+    /**
+     * Tuple values.
+     */
+    private final String[] values;
+
+    /**
+     * Constructor.
+     * @param name Tuple name.
+     * @param values Tuple values.
+     */
+    public DirectivesTuple(
+        final String name,
+        final String[] values
+    ) {
+        this.name = name;
+        this.values = values;
+    }
+
+    @Override
+    public Iterator<Directive> iterator() {
+        final Directives tuple = new Directives()
+            .add("o")
+            .attr("base", "tuple")
+            .attr("data", "tuple")
+            .attr("name", this.name);
+        for (final String exception : this.values) {
+            tuple.append(new DirectivesData(exception).directives());
+        }
+        tuple.up();
+        return tuple.iterator();
+    }
+}
