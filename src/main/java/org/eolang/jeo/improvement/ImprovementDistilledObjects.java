c70575e8f30041229be659f5dc34421ccb58b1cd
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-10-23 15:57:22 +0300

    feat(#167): remove redundant methods

--- src/main/java/org/eolang/jeo/improvement/ImprovementDistilledObjects.java
+++ src/main/java/org/eolang/jeo/improvement/ImprovementDistilledObjects.java
@@ -34,7 +34,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.eolang.jeo.Improvement;
