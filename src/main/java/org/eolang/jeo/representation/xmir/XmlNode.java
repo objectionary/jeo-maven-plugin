be8fbcbfbcc820150088db9847cf0d8171e9ff16
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-10-25 17:01:32 +0300

    feat(#193): add one more puzzle for unit tests

--- src/main/java/org/eolang/jeo/representation/xmir/XmlNode.java
+++ src/main/java/org/eolang/jeo/representation/xmir/XmlNode.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2023 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.eolang.jeo.representation.xmir;
 
 import com.jcabi.xml.XMLDocument;
@@ -6,6 +29,11 @@ import org.w3c.dom.NodeList;
 
 /**
  * XML smart element.
+ * Utility class that simplifies work with XML.
+ * @since 0.1
+ * @todo #193:90min Add unit tests for XmlNode.
+ *  Currently we don't have unit tests for XmlNode. So, it makes sense to add
+ *  them to keep code safe and clear.
  */
 final class XmlNode {
 
@@ -14,10 +42,19 @@ final class XmlNode {
      */
     private final Node node;
 
-    public XmlNode(final Node parent) {
+    /**
+     * Constructor.
+     * @param parent Parent node.
+     */
+    XmlNode(final Node parent) {
         this.node = parent;
     }
 
+    /**
+     * Get child node.
+     * @param name Child node name.
+     * @return Child node.
+     */
     XmlNode child(final String name) {
         final NodeList children = this.node.getChildNodes();
         for (int index = 0; index < children.getLength(); ++index) {
@@ -29,10 +66,18 @@ final class XmlNode {
         throw this.notFound(name);
     }
 
+    /**
+     * Convert to class.
+     * @return Class.
+     */
     XmlClass toClass() {
         return new XmlClass(this.node);
     }
 
+    /**
+     * Clean all child nodes.
+     * @return The same node.
+     */
     XmlNode clean() {
         while (this.node.hasChildNodes()) {
             this.node.removeChild(this.node.getFirstChild());
@@ -40,11 +85,21 @@ final class XmlNode {
         return this;
     }
 
-    XmlNode append(final Node node) {
-        this.node.appendChild(this.node.getOwnerDocument().adoptNode(node.cloneNode(true)));
+    /**
+     * Append child node.
+     * @param child Node to append.
+     * @return The same node.
+     */
+    XmlNode append(final Node child) {
+        this.node.appendChild(this.node.getOwnerDocument().adoptNode(child.cloneNode(true)));
         return this;
     }
 
+    /**
+     * Generate exception if element not found.
+     * @param name Element name.
+     * @return Exception.
+     */
     private IllegalStateException notFound(final String name) {
         return new IllegalStateException(
             String.format(
