657b0070f26b01250a66733d67cc7f7ee7febee7
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-10-25 17:53:30 +0300

    feat(#188): Add new puzzle

--- src/it/generics/verify.groovy
+++ src/it/generics/verify.groovy
@@ -32,4 +32,38 @@ assert log.contains("Roman likes write code in Kotlin")
 //assert new File(basedir, 'target/jeo/xmir/org/eolang/jeo/Application.xmir').exists()
 //assert new File(basedir, 'target/jeo/xmir/org/eolang/jeo/Developer.xmir').exists()
 //assert new File(basedir, 'target/jeo/xmir/org/eolang/jeo/Language.xmir').exists()
+/**
+ * @todo #188:30min Enable Integration Test for Generics.
+ *  First of all, we have to implement Java Generics support in JEO.
+ *  Then, we need to add JEO plugin into pom.xml of that test, the
+ *  plugin setup you can find below. After that we need to add some
+ *  assertions to this file. For example, we need to check that we have
+ *  generated EO object files (see commented checks above).
+ *
+ * <p>
+ * {@code
+ * <plugin>
+ *   <groupId>org.eolang</groupId>
+ *   <artifactId>jeo-maven-plugin</artifactId>
+ *   <version>@project.version@</version>
+ *   <executions>
+ *     <execution>
+ *       <id>bytecode-to-eo</id>
+ *       <goals>
+ *         <goal>bytecode-to-eo</goal>
+ *       </goals>
+ *     </execution>
+ *     <execution>
+ *       <id>eo-to-bytecode</id>
+ *       <goals>
+ *         <goal>eo-to-bytecode</goal>
+ *       </goals>
+ *     </execution>
+ *   </executions>
+ * </plugin>
+ *
+ *}
+ * </p>
+ */
+
 true
