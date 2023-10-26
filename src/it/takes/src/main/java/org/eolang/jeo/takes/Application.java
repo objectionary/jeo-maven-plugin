0909b757a2927926b9578ec5503a238a8ce36e5a
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-10-26 16:20:43 +0300

    feat(#189): make it pass

--- src/it/takes/src/main/java/org/eolang/jeo/takes/Application.java
+++ src/it/takes/src/main/java/org/eolang/jeo/takes/Application.java
@@ -1,7 +1,7 @@
-package org.eolang.jeo;
+package org.eolang.jeo.takes;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello from Takes Framework!");
     }
 }
--- src/it/takes/verify.groovy
+++ src/it/takes/verify.groovy
@@ -24,10 +24,7 @@
 //Check logs first.
 String log = new File(basedir, 'build.log').text;
 assert log.contains("BUILD SUCCESS")
-assert log.contains("Yegor likes write code in EO")
-assert log.contains("Lev likes write code in Rust")
-assert log.contains("Maxim likes write code in PHP")
-assert log.contains("Roman likes write code in Kotlin")
+assert log.contains("Hello from Takes Framework!")
 //Check that we have generated EO object files.
 //assert new File(basedir, 'target/jeo/xmir/org/eolang/jeo/Application.xmir').exists()
 //assert new File(basedir, 'target/jeo/xmir/org/eolang/jeo/Developer.xmir').exists()
