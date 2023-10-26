47b946b7cdeb1b88e68ad1efa1a7028abd0c3e78
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-10-26 16:47:34 +0300

    feat(#189): Add more classes for Takes Framework

--- src/it/takes/src/main/java/org/eolang/jeo/takes/Application.java
+++ src/it/takes/src/main/java/org/eolang/jeo/takes/Application.java
@@ -1,7 +1,59 @@
 package org.eolang.jeo.takes;
 
+import org.takes.Request;
+import org.takes.Response;
+import org.takes.facets.fork.Fork;
+import org.takes.http.Exit;
+import org.takes.http.FtBasic;
+import org.takes.facets.fork.FkRegex;
+import org.takes.facets.fork.TkFork;
+import java.io.IOException;
+import org.takes.misc.Opt;
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello from Takes Framework!");
+    public static void main(String[] args) throws IOException {
+        new FtBasic(
+            new TkFork(
+                new RequestCounter(
+                    new TimeLog(
+                        new FkRegex("/", "It is the Takes Framework"))
+                )
+            ),
+            8080
+        ).start(Exit.NEVER);
     }
+
+    private static class TimeLog implements Fork {
+
+        private final Fork origin;
+
+        TimeLog(final Fork origin) {
+            this.origin = origin;
+        }
+
+        @Override
+        public Opt<Response> route(final Request req) throws Exception {
+            System.out.printf("Request time: %d%n", System.currentTimeMillis());
+            return this.origin.route(req);
+        }
+    }
+
+    private static class RequestCounter implements Fork {
+
+        private final Fork origin;
+        private int counter = 0;
+
+        RequestCounter(final Fork origin) {
+            this.origin = origin;
+        }
+
+        @Override
+        public Opt<Response> route(final Request req) throws Exception {
+            this.counter += 1;
+            System.out.printf("Request #%d%n", this.counter);
+            return this.origin.route(req);
+        }
+    }
+
+
 }
