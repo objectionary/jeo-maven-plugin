9ddb86d0923f04e29f698a360779e85224d33878
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-10-31 18:18:32 +0300

    feat(#214): fix all integration tests

--- src/it/generics/verify.groovy
+++ src/it/generics/verify.groovy
@@ -29,9 +29,9 @@ assert log.contains("Lev likes write code in Rust")
 assert log.contains("Maxim likes write code in PHP")
 assert log.contains("Roman likes write code in Kotlin")
 //Check that we have generated EO object files.
-//assert new File(basedir, 'target/jeo/xmir/org/eolang/jeo/Application.xmir').exists()
-//assert new File(basedir, 'target/jeo/xmir/org/eolang/jeo/Developer.xmir').exists()
-//assert new File(basedir, 'target/jeo/xmir/org/eolang/jeo/Language.xmir').exists()
+//assert new File(basedir, 'target/generated-sources/xmir/org/eolang/jeo/Application.xmir').exists()
+//assert new File(basedir, 'target/generated-sources/xmir/org/eolang/jeo/Developer.xmir').exists()
+//assert new File(basedir, 'target/generated-sources/xmir/org/eolang/jeo/Language.xmir').exists()
 /**
  * @todo #188:30min Enable Integration Test for Generics.
  *  First of all, we have to implement Java Generics support in JEO.
