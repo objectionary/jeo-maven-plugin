e8e6dd0dad1ad6cdc17137a55ad4753171c04700
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-11-03 17:58:49 +0300

    feat(#226): add one more puzzle

--- src/main/java/org/eolang/jeo/representation/xmir/XmlCommand.java
+++ src/main/java/org/eolang/jeo/representation/xmir/XmlCommand.java
@@ -2,6 +2,13 @@ package org.eolang.jeo.representation.xmir;
 
 import org.eolang.jeo.representation.bytecode.BytecodeMethod;
 
+/**
+ * XML representation of bytecode instruction or a label.
+ * @todo #226:90min Rename XmlCommand to XmlInstruction.
+ *  XmlCommand is a bad name for this class. It is not a command, it is an instruction in general.
+ *  Since instruction could be a label or a bytecode instruction, it is better to rename this class
+ *  to XmlInstruction. After that, rename all usages of the old XmlInstruction to XmlCommand.
+ */
 public interface XmlCommand {
 
     void writeTo(BytecodeMethod method);
