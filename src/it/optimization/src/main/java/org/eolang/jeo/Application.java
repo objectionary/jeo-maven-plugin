09cd6436b9d57f40dd92a8a0f489725d0d04436a
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-11-07 14:21:05 +0300

    feat(#234): add conditional statement for optimization integration test

--- src/it/optimization/src/main/java/org/eolang/jeo/Application.java
+++ src/it/optimization/src/main/java/org/eolang/jeo/Application.java
@@ -2,6 +2,17 @@ package org.eolang.jeo;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        if (bar(1.0d) > 7) {
+            System.out.println("Hello, World!");
+        } else {
+            System.out.println("Wake up, Neo...");
+        }
+    }
+
+    private static int bar(double x) {
+        if (x > 0.0d) {
+            return 5;
+        }
+        return 8;
     }
 }
