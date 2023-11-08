1cda7a32bb207b4ccf9864b9f75970ad4be8f617
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-11-08 10:21:33 +0300

    feat(#165): add integration test changes that shows what we want to achieve

--- src/it/distilled/src/main/java/org/eolang/jeo/B.java
+++ src/it/distilled/src/main/java/org/eolang/jeo/B.java
@@ -1,13 +1,14 @@
 package org.eolang.jeo;
 
-public final class B{
+public final class B {
     private final A a;
+    private int x = 2;
 
-    B(A a){
+    B(A a) {
         this.a = a;
     }
 
-    int bar(){
-        return a.foo() + 2;
+    int bar() {
+        return a.foo() + x;
     }
 }
