64bab3c259debfcf6483c20192159cf00347a4d6
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-11-21 19:45:33 +0300

    feat(#269): add javadocs for Details class

--- src/main/java/org/eolang/jeo/Details.java
+++ src/main/java/org/eolang/jeo/Details.java
@@ -1,27 +1,86 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2023 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.eolang.jeo;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Details of representation.
+ * Additional info about representation.
+ * @since 0.1
+ */
 public class Details {
 
+    /**
+     * Name of the class or an object.
+     */
     private static final String NAME = "name";
+
+    /**
+     * Original source of the representation.
+     * Might be a file name or a URL:
+     * - Application.java
+     * - Application.xmir
+     */
     private static final String SOURCE = "source";
+
+    /**
+     * Storage with all the details.
+     */
     private final Map<String, String> storage;
 
-    public Details(final String name) {
-        this(name, "Unknown");
-    }
-
+    /**
+     * Constructor.
+     * @param name Name of the class or an object.
+     * @param source Original source of the representation.
+     */
     public Details(final String name, final String source) {
         this(Details.NAME, name, Details.SOURCE, source);
     }
 
-    public Details(final String... inits) {
+    /**
+     * Constructor.
+     * @param name Name of the class or an object.
+     */
+    Details(final String name) {
+        this(name, "Unknown");
+    }
+
+    /**
+     * Constructor.
+     * @param inits Initializations.
+     */
+    private Details(final String... inits) {
         this(Details.initial(inits));
     }
 
-    public Details(final Map<String, String> storage) {
+    /**
+     * Constructor.
+     * @param storage Storage with all the details.
+     */
+    private Details(final Map<String, String> storage) {
         this.storage = storage;
     }
 
@@ -42,6 +101,11 @@ public class Details {
         return this.storage.get(Details.SOURCE);
     }
 
+    /**
+     * Initializations.
+     * @param pairs Pairs of key-value.
+     * @return Map with all the details.
+     */
     private static Map<String, String> initial(String... pairs) {
         final int length = pairs.length;
         if (length % 2 == 1) {
@@ -53,6 +117,4 @@ public class Details {
         }
         return map;
     }
-
-
 }
