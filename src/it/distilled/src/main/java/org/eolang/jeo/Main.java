69f710cfb136a02594b8214e2d470964cba3fdfa
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-11-22 16:05:48 +0300

    feat(#204): remove 'distilled objects' optimization

--- src/it/distilled/src/main/java/org/eolang/jeo/Main.java
+++
@@ -1,8 +0,0 @@
-package org.eolang.jeo;
-
-public class Main {
-    public static void main(String[] args) {
-        int x = new B(new A(28)).bar();
-        System.out.println("Result: " + x);
-    }
-}
