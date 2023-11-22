511bd02e905bba8899d30ce58c4d33aad2e6e52f
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-11-22 16:07:13 +0300

    feat(#204): repair all unit tests

--- src/main/java/org/eolang/jeo/JeoMojo.java
+++ src/main/java/org/eolang/jeo/JeoMojo.java
@@ -31,7 +31,6 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.eolang.jeo.improvement.ImprovementBytecodeFootprint;
-import org.eolang.jeo.improvement.ImprovementDistilledObjects;
 import org.eolang.jeo.improvement.ImprovementEoFootprint;
 import org.eolang.jeo.improvement.ImprovementLogged;
 import org.eolang.jeo.improvement.ImprovementSet;
@@ -72,7 +71,6 @@ public final class JeoMojo extends AbstractMojo {
                 this.classes.toPath(),
                 new ImprovementSet(
                     new ImprovementLogged(),
-                    new ImprovementDistilledObjects(),
                     new ImprovementXmirFootprint(this.generated.toPath()),
                     new ImprovementEoFootprint(this.generated.toPath()),
                     new ImprovementBytecodeFootprint(this.classes.toPath())
