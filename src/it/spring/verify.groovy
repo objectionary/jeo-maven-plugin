62af3274209b274e8a5556a0aa6f2118de745742
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-12-18 17:28:31 +0300

    feat(#292): remove the 292 puzzle description

--- src/it/spring/verify.groovy
+++ src/it/spring/verify.groovy
@@ -26,40 +26,6 @@ String log = new File(basedir, 'build.log').text;
 assert log.contains("BUILD SUCCESS")
 assert log.contains("Glad to see you, Spring...")
 //Check that we have generated EO object files.
-//assert new File(basedir, 'target/generated-sources/xmir/org/eolang/jeo/spring/Application.xmir').exists()
-//assert new File(basedir, 'target/generated-sources/xmir/org/eolang/jeo/spring/Receptionist.xmir').exists()
-/**
- * @todo #189:30min Enable Integration Test for Spring Framework.
- *  First of all, we have to implement all the features required for correct
- *  transformation. Then, we need to add JEO plugin into pom.xml of that test,
- *  the plugin setup you can find below. After that we need to add some
- *  assertions to this file. For example, we need to check that we have
- *  generated EO object files (see commented checks above).
- *
- * <p>
- * {@code
- * <plugin>
- *   <groupId>org.eolang</groupId>
- *   <artifactId>jeo-maven-plugin</artifactId>
- *   <version>@project.version@</version>
- *   <executions>
- *     <execution>
- *       <id>bytecode-to-eo</id>
- *       <goals>
- *         <goal>bytecode-to-eo</goal>
- *       </goals>
- *     </execution>
- *     <execution>
- *       <id>eo-to-bytecode</id>
- *       <goals>
- *         <goal>eo-to-bytecode</goal>
- *       </goals>
- *     </execution>
- *   </executions>
- * </plugin>
- *
- *}
- * </p>
- */
-
+assert new File(basedir, 'target/generated-sources/xmir/org/eolang/jeo/spring/Application.xmir').exists()
+assert new File(basedir, 'target/generated-sources/xmir/org/eolang/jeo/spring/Receptionist.xmir').exists()
 true
