5552e7a4db3778f864457d4a16d4647155be7a61
rultor <me@rultor.com>
2024-02-07 13:08:07 +0000

    Deploying to gh-pages from @ objectionary/jeo-maven-plugin@dffbe345e2d503e09dc741836e999f0d8fbf30f8 🚀

---
+++ 0.3.2/apidocs-0.3.2/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.eolang.jeo"},{"l":"org.eolang.jeo.representation"},{"l":"org.eolang.jeo.representation.bytecode"},{"l":"org.eolang.jeo.representation.directives"},{"l":"org.eolang.jeo.representation.xmir"}]
