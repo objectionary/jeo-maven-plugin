2b762222b4506b5c6d8d2916e4f1ccfa587e794a
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-08-08 11:34:54 +0300

    feat(#661): fix annotation names

--- src/main/java/org/eolang/jeo/representation/directives/DirectivesAnnotation.java
+++ src/main/java/org/eolang/jeo/representation/directives/DirectivesAnnotation.java
@@ -84,7 +84,10 @@ public final class DirectivesAnnotation implements Iterable<Directive>, Composit
     public Iterator<Directive> iterator() {
         final Directives directives = new Directives().add("o")
             .attr("base", "annotation")
-            .attr("name", new MethodName("annotation", this.descriptor).encoded())
+            .attr("name", new MethodName(
+                String.format("annotation-%d", new Random().nextInt(Integer.MAX_VALUE)),
+                this.descriptor
+            ).encoded())
             .attr("line", new Random().nextInt(Integer.MAX_VALUE))
             .append(new DirectivesData(this.descriptor))
             .append(new DirectivesData(this.visible));
