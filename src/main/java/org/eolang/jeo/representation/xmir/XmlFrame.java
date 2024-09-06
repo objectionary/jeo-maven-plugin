2fa34a24183f77dc4c282764620a7d0a151adc3a
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-09-06 16:56:13 +0300

    feat(#636): remove redundant code

--- src/main/java/org/eolang/jeo/representation/xmir/XmlFrame.java
+++ src/main/java/org/eolang/jeo/representation/xmir/XmlFrame.java
@@ -82,7 +82,7 @@ public final class XmlFrame implements XmlBytecodeEntry {
 
     @Override
     public void writeTo(final BytecodeMethod method) {
-        method.entry(new FrameEntry());
+        method.entry(this.bytecode());
     }
 
     /**
@@ -137,35 +137,5 @@ public final class XmlFrame implements XmlBytecodeEntry {
             .map(XmlOperand::new)
             .map(XmlOperand::asObject)
             .toArray(Object[]::new);
-    }
-
-    /**
-     * Frame entry.
-     *
-     * @since 0.3
-     */
-    private final class FrameEntry implements BytecodeEntry {
-        @Override
-        public void writeTo(final MethodVisitor visitor) {
-            visitor.visitFrame(
-                XmlFrame.this.type(),
-                XmlFrame.this.nlocal(),
-                XmlFrame.this.locals(),
-                XmlFrame.this.nstack(),
-                XmlFrame.this.stack()
-            );
-        }
-
-        @Override
-        public String testCode() {
-            return String.format(
-                ".visitFrame(%d, %d, new Object[]{ %s }, %d, new Object[]{ %s })",
-                XmlFrame.this.type(),
-                XmlFrame.this.nlocal(),
-                Arrays.toString(XmlFrame.this.locals()),
-                XmlFrame.this.nstack(),
-                Arrays.toString(XmlFrame.this.stack())
-            );
-        }
     }
 }
