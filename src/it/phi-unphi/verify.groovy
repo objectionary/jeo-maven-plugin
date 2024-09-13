5079c2739b5737e7d7f47b76dacc7e307ff9569b
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-09-13 14:31:50 +0300

    feat(#703): add informative error message in case assertion fails

--- src/it/phi-unphi/verify.groovy
+++ src/it/phi-unphi/verify.groovy
@@ -22,6 +22,14 @@
  * SOFTWARE.
  */
 String log = new File(basedir, 'build.log').text;
-assert log.contains("BUILD SUCCESS") : "BUILD FAILED"
-assert log.contains("sin(42.000000) = -0.916522") : "sin() not found"
+assert log.contains("BUILD SUCCESS"): assertionMessage("BUILD FAILED")
+assert log.contains("sin(42.000000) = -0.916522"): assertionMessage("sin(42.000000) = -0.916522 not found")
+
+private String assertionMessage(String message) {
+    return String.format(
+      "'%s', you can find the entire log in the 'file://%s' file",
+      message,
+      new File(basedir, 'build.log').absolutePath)
+}
+
 true
