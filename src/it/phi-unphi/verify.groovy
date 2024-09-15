d484418e59b5768fe83cdc96366dea44eaab6f9c
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-09-15 12:40:25 +0300

    feat(#703): try to generate github issue based on failed integration test

--- src/it/phi-unphi/verify.groovy
+++ src/it/phi-unphi/verify.groovy
@@ -26,10 +26,79 @@ assert log.contains("BUILD SUCCESS"): assertionMessage("BUILD FAILED")
 assert log.contains("sin(42.000000) = -0.916522"): assertionMessage("sin(42.000000) = -0.916522 not found")
 
 private String assertionMessage(String message) {
+    generateGitHubIssue()
     return String.format(
       "'%s', you can find the entire log in the 'file://%s' file",
       message,
       new File(basedir, 'build.log').absolutePath)
+}
+
+private void generateGitHubIssue() {
+    new File(basedir, 'build.log').text
+
+    copy('target/generated-sources/jeo-disassemble/org/eolang/hone/App.xmir', 'App.xmir.disassemble.txt')
+    copy('target/generated-sources/eo-phi/org/eolang/hone/App.phi', 'App.phi.txt')
+    copy('target/generated-sources/eo-unphi/org/eolang/hone/App.xmir', 'App.xmir.unphi.txt')
+
+    def issue = '''
+ I run the following [integration test](https://github.com/objectionary/jeo-maven-plugin/tree/master/src/it/phi-unphi):
+ 
+ bytecode -> (disassemble) `xmir`  -> `phi` -> `unphi` -> `xmir` (assemble) -> bytecode.
+ 
+ And this test fails because `phi/unphi` alter the original `xmir` file.
+ 
+ **Steps to reproduce:**
+ 
+ 1) I generate `App.xmir` from `App.class` file (`jeo:disassemble`):
+ [App.xmir.disassemble.txt](todo)
+ 2) Then I use `eo:0.39.0:xmir-to-phi` to generate `App.phi`:
+ [App.phi.txt](todo)
+ 3) Then I run `eo:0.39.0:phi-to-xmir` to generate `App.xmir` (and it is generated):
+ [App.xmir.unphi.txt](todo)
+ 
+ **Expected behaviour:**
+ 
+ `App.xmir.disassemble.txt` and `App.xmir.unphi.txt` files should be the same. In other words, `phi/unphi` does not
+ have to change the original `xmir` file.
+ 
+ **Actual behaviour:** 
+ `App.xmir.disassemble.txt` and `App.xmir.unphi.txt` files are different. `phi/unphi` significantly changes the original `xmir`.
+ 
+ **Details:**
+    '''
+
+    // Generate the Bash script
+    def bashScript = """#!/bin/bash
+
+# Attachments
+file1="App.xmir.disassemble.txt"
+file2="App.phi.txt"
+file3="App.xmir.unphi.txt"
+
+# Create the issue using the 'gh' CLI
+gh issue create \\
+    --title "Integration Test Failure: `phi/unphi` alters xmir file" \\
+    --body '$issue' \\
+    --label "bug" \\
+    --repo volodya-lombrozo/eo \\
+    --attach \$file1 \\
+    --attach \$file2 \\
+    --attach \$file3
+    """
+
+    def file = new File(basedir, "create_issue.sh")
+    file.text = bashScript
+    println "Bash script generated: file://${basedir}/create_issue.sh"
+}
+
+private void copy(final String source, final String destination) {
+    def sourceFile = new File(basedir, source)
+    def destinationFile = new File(basedir, destination)
+    if (sourceFile.renameTo(destinationFile)) {
+        println "'${source}' moved and renamed to '${destination}'!"
+    } else {
+        println "Failed to move the '${source}' file to '${destination}'."
+    }
 }
 
 true
