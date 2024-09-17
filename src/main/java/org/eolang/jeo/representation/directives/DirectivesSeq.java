032c0b3aa0473a0104becd5d2afcb2650bb5bb34
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-09-17 10:26:19 +0300

    feat(#707): add DirectivesSeq class

---
+++ src/main/java/org/eolang/jeo/representation/directives/DirectivesSeq.java
@@ -0,0 +1,100 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2024 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.eolang.jeo.representation.directives;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+import org.xembly.Directive;
+import org.xembly.Directives;
+
+/**
+ * Directives sequence.
+ * @since 0.6
+ */
+public final class DirectivesSeq implements Iterable<Directive> {
+
+    private final String name;
+
+    /**
+     * Directives.
+     */
+    private final List<? extends Iterable<Directive>> directives;
+
+    /**
+     * Constructor.
+     * @param elements Elements to wrap.
+     */
+    public DirectivesSeq(Iterable<Directive>... elements) {
+        this("@", elements);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param name Name of the sequence.
+     * @param elements Elements to wrap.
+     */
+    @SafeVarargs
+    public DirectivesSeq(final String name, Iterable<Directive>... elements) {
+        this(name, Arrays.asList(elements));
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param name Name of the sequence.
+     * @param elements Elements to wrap.
+     */
+    public DirectivesSeq(final String name, final List<? extends Iterable<Directive>> elements) {
+        this.name = name;
+        this.directives = elements;
+    }
+
+
+    @Override
+    public Iterator<Directive> iterator() {
+        return new Directives()
+            .add("o")
+            .attr("base", String.format("seq%d", this.size()))
+            .attr("line", new Random().nextInt(Integer.MAX_VALUE))
+            .attr("name", this.name)
+            .append(
+                this.directives.stream()
+                    .filter(Objects::nonNull)
+                    .map(Directives::new)
+                    .reduce(new Directives(), Directives::append)
+            ).up().iterator();
+    }
+
+    /**
+     * Size of the sequence.
+     * @return Size.
+     */
+    private int size() {
+        return this.directives.size();
+    }
+}
---
+++ src/test/java/org/eolang/jeo/representation/directives/DirectivesSeqTest.java
@@ -0,0 +1,54 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2024 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.eolang.jeo.representation.directives;
+
+import com.jcabi.matchers.XhtmlMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+import org.xembly.ImpossibleModificationException;
+import org.xembly.Xembler;
+
+/**
+ * Directives sequence test cases.
+ * Test cases for {@link DirectivesSeq}.
+ * @since 0.6
+ */
+final class DirectivesSeqTest {
+
+    @Test
+    void convertsToNumberedSeq() throws ImpossibleModificationException {
+        MatcherAssert.assertThat(
+            "We expect to get a numbered sequence with two elements",
+            new Xembler(
+                new DirectivesSeq(
+                    new DirectivesData("1"), new DirectivesData("2"))
+            ).xml(),
+            XhtmlMatchers.hasXPaths(
+                "/o[@base='seq2' and @name='@']",
+                "/o[@base='seq2' and @name='@']/o[@base='string' and @data='bytes' and text()='31']",
+                "/o[@base='seq2' and @name='@']/o[@base='string' and @data='bytes' and text()='32']"
+            )
+        );
+    }
+}
