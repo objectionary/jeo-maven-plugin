86abe58dfa11fb3f6c6aca07180b0ad45e53f2b2
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-09-17 12:35:16 +0300

    feat(#707): add DirectivesValues

---
+++ src/main/java/org/eolang/jeo/representation/directives/DirectivesValues.java
@@ -0,0 +1,64 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2024 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.eolang.jeo.representation.directives;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.stream.Collectors;
+import org.xembly.Directive;
+
+/**
+ * Array representation of some values as Xembly directives.
+ * We used to use "tuple" for this, but it leads to some confusion.
+ * You can read more about this problem
+ * <a href="https://github.com/objectionary/jeo-maven-plugin/issues/707">here</a>
+ * @since 0.6
+ */
+public final class DirectivesValues implements Iterable<Directive> {
+
+    /**
+     * Tuple name.
+     */
+    private final String name;
+
+    /**
+     * Tuple values.
+     */
+    private final Object[] values;
+
+    public DirectivesValues(final String name, final Object... values) {
+        this.name = name;
+        this.values = values;
+    }
+
+    @Override
+    public Iterator<Directive> iterator() {
+        return new DirectivesSeq(
+            this.name,
+            Arrays.stream(this.values)
+                .map(DirectivesData::new)
+                .collect(Collectors.toList())
+        ).iterator();
+    }
+}
---
+++ src/test/java/org/eolang/jeo/representation/directives/DirectivesValuesTest.java
@@ -0,0 +1,50 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2024 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.eolang.jeo.representation.directives;
+
+import com.jcabi.matchers.XhtmlMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+import org.xembly.ImpossibleModificationException;
+import org.xembly.Xembler;
+
+/**
+ * Test case for {@link DirectivesValues}.
+ * @since 0.6
+ */
+final class DirectivesValuesTest {
+
+    @Test
+    void convertsToXmir() throws ImpossibleModificationException {
+        MatcherAssert.assertThat(
+            "We expect that array of values will be converted to correct Xembly directives",
+            new Xembler(new DirectivesValues("name", "value")).xml(),
+            XhtmlMatchers.hasXPaths(
+                "./o[@base='seq1' and @name='name']",
+                "./o[@base='seq1' and @name='name']/o[@data='bytes' and text()='76 61 6C 75 65']"
+            )
+        );
+    }
+
+}
