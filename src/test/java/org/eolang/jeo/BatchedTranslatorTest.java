dedaae05242d158868bbc5a9d97b91930e6e5c26
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-11-18 16:51:20 +0300

    feat(#869): make all the tests pass

--- src/test/java/org/eolang/jeo/BatchedTranslatorTest.java
+++ src/test/java/org/eolang/jeo/BatchedTranslatorTest.java
@@ -30,7 +30,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.eolang.jeo.representation.XmirRepresentation;
 import org.eolang.jeo.representation.bytecode.BytecodeClass;
 import org.eolang.jeo.representation.bytecode.BytecodeProgram;
 import org.hamcrest.MatcherAssert;
@@ -62,9 +61,7 @@ final class BatchedTranslatorTest {
             new BytecodeProgram(
                 "org/eolang/jeo",
                 new BytecodeClass("Application")
-            ).xml()
-                .toString()
-                .getBytes(StandardCharsets.UTF_8)
+            ).bytecode().bytes()
         );
         new BatchedTranslator(new Disassemble(temp))
             .apply(Stream.of(clazz))
@@ -85,7 +82,7 @@ final class BatchedTranslatorTest {
             new BytecodeProgram(
                 "org/eolang/jeo",
                 new BytecodeClass("Application")
-            ).xml().toString().getBytes(StandardCharsets.UTF_8)
+            ).bytecode().bytes()
         );
         final BatchedTranslator footprint = new BatchedTranslator(
             new Disassemble(temp)
@@ -113,9 +110,9 @@ final class BatchedTranslatorTest {
                 new BytecodeClass(fake)
             ).xml().toString().getBytes(StandardCharsets.UTF_8)
         );
-        new BatchedTranslator(new Assemble(temp)).apply(
-            Stream.of(path)
-        ).collect(Collectors.toList());
+        new BatchedTranslator(new Assemble(temp))
+            .apply(Stream.of(path))
+            .collect(Collectors.toList());
         MatcherAssert.assertThat(
             String.format(
                 "Bytecode file was not saved for the representation with the name '%s'",
