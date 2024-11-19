06da3f72f3195196e58549a4474dcaad335cb3a7
h1alexbel <h1alexbelx@gmail.com>
2024-11-19 15:32:01 +0300

    feat(#546): objects POC off

--- src/main/java/org/eolang/jeo/representation/xmir/XmlNode.java
+++ src/main/java/org/eolang/jeo/representation/xmir/XmlNode.java
@@ -33,6 +33,7 @@ import java.util.stream.Stream;
 import org.eolang.jeo.representation.directives.JeoFqn;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * XML smart element.
@@ -259,11 +260,12 @@ public final class XmlNode {
      * Objects.
      * @return Stream of class objects.
      */
-    private Stream<XML> objects() {
-        final List<XML> nodes = new XMLDocument(this.node).nodes("node()");
-        final List<XML> res = new ArrayList<>(nodes.size());
-        for (final XML child : nodes) {
-            if ("o".equals(child.node().getNodeName())) {
+    private Stream<Node> objects() {
+        final NodeList children = this.node.getChildNodes();
+        final List<Node> res = new ArrayList<>(children.getLength());
+        for (int index = 0; index < children.getLength(); ++index) {
+            final Node child = children.item(index);
+            if ("o".equals(child.getNodeName())) {
                 res.add(child);
             }
         }
