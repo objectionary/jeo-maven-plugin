ad98b8152821cd8f67730ceab8d5d4d66b8d8062
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-11-20 15:43:06 +0300

    feat(#501): fix a few more unit tests

--- src/test/java/org/eolang/jeo/representation/xmir/XmlInstructionTest.java
+++ src/test/java/org/eolang/jeo/representation/xmir/XmlInstructionTest.java
@@ -44,10 +44,13 @@ final class XmlInstructionTest {
 
     @Test
     void comparesSuccessfullyWithSpaces() {
+        final BytecodeInstruction bytecode = new XmlInstruction(
+            false, Opcodes.INVOKESPECIAL, 1, 2, 3).bytecode();
+        final BytecodeInstruction inst = XmlInstructionTest.INST;
         MatcherAssert.assertThat(
             "Xml Instruction nodes with different empty spaces, but with the same content should be the same, but it wasn't",
-            new XmlInstruction(false, Opcodes.INVOKESPECIAL, 1, 2, 3).bytecode(),
-            Matchers.equalTo(XmlInstructionTest.INST)
+            bytecode,
+            Matchers.equalTo(inst)
         );
     }
 
