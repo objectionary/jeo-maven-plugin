0be896a916378caac9dcaa2a77819a71bbab9828
l3r8yJ <l3r8y@duck.com>
2024-12-08 05:27:14 +0300

    feat(#909): move  to class; read from resources

--- src/main/java/org/eolang/jeo/representation/directives/DirectivesProgram.java
+++ src/main/java/org/eolang/jeo/representation/directives/DirectivesProgram.java
@@ -28,6 +28,7 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Iterator;
+import org.eolang.jeo.representation.License;
 import org.xembly.Directive;
 import org.xembly.Directives;
 
@@ -117,48 +118,12 @@ public final class DirectivesProgram implements Iterable<Directive> {
             .add("listing")
             .set(this.listing)
             .up()
-            .add("license").set(DirectivesProgram.license()).up()
+            .add("license").set(new License()).up()
             .append(this.metas)
             .attr("ms", this.milliseconds)
             .add("objects");
         directives.append(this.klass);
         directives.up();
         return directives.iterator();
-    }
-
-    /**
-     * Text of the license.
-     * @return License text.
-     * @todo #850 Retrieve License From The File.
-     *  We use license as a file in the project root.
-     *  See LICENSE.txt file.
-     *  It's better to read this file and return its content.
-     *  Otherwise, we have to maintain the license in two places.
-     */
-    private static String license() {
-        return String.join(
-            "\n",
-            "The MIT License (MIT)",
-            "",
-            "Copyright (c) 2016-2024 Objectionary.com",
-            "",
-            "Permission is hereby granted, free of charge, to any person obtaining a copy",
-            "of this software and associated documentation files (the \"Software\"), to deal",
-            "in the Software without restriction, including without limitation the rights",
-            "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell",
-            "copies of the Software, and to permit persons to whom the Software is",
-            "furnished to do so, subject to the following conditions:",
-            "",
-            "The above copyright notice and this permission notice shall be included",
-            "in all copies or substantial portions of the Software.",
-            "",
-            "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR",
-            "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,",
-            "FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE",
-            "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER",
-            "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,",
-            "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE",
-            "SOFTWARE."
-        );
     }
 }
