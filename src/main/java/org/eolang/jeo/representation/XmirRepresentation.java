8c47881137aafb49d9a88054a8915820ae5b4bd1
volodya-lombrozo <volodya.lombrozo@gmail.com>
2025-01-16 10:03:39 +0300

    feat(#967): fix the bug

--- src/main/java/org/eolang/jeo/representation/XmirRepresentation.java
+++ src/main/java/org/eolang/jeo/representation/XmirRepresentation.java
@@ -83,7 +83,10 @@ public final class XmirRepresentation {
     public String name() {
         final XmlNode root = this.xml.root();
         return new ClassName(
-            root.xpath("/program/metas/meta[head[text()]='package']/tail/text()").get(0),
+            root.xpath("/program/metas/meta[head[text()]='package']/tail/text()")
+                .stream()
+                .findFirst()
+                .orElse(""),
             root.xpath("/program/@name").get(0)
         ).full();
     }
