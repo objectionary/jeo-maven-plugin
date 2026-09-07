8977d07fe6e9114a4b726ea4850020db98215055
VasilevNStas <ctac@outlook.com>
2026-09-07 21:07:31 +0300

    test(#1617): add integration tests for the mojo parameters and the negative threads value

---
+++ src/it/mojo-parameters/src/main/java/org/eolang/jeo/params/Application.java
@@ -0,0 +1,20 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2016-2026 Objectionary.com
+ * SPDX-License-Identifier: MIT
+ */
+package org.eolang.jeo.params;
+
+/**
+ * Simple class for the mojo parameters integration test.
+ * @since 0.1
+ */
+public final class Application {
+
+    /**
+     * Entry point.
+     * @param args Arguments.
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello from mojo parameters test!");
+    }
+}
