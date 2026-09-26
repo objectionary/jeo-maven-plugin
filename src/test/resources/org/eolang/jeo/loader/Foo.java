ca9eedb625f4e553e21ef3895c193440ce3abd1c
devil <europedreadlydevil@gmail.com>
2026-09-26 06:56:41 +0000

    #1762: add regression test for JeoClassLoader in a package named classes

---
+++ src/test/resources/org/eolang/jeo/loader/Foo.java
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2016-2026 Objectionary.com
+ * SPDX-License-Identifier: MIT
+ */
+package com.acme.classes;
+
+/**
+ * Trivial class in a package whose name contains ".class" (see #1762).
+ */
+public final class Foo {
+}
